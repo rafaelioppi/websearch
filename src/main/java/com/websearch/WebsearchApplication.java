30ae8f25a5a746c4a3ff6f521282475f179f960a
rafaelioppi <141973185+rafaelioppi@users.noreply.github.com>
2026-04-29 17:15:58 +0000

    rafael

---
+++ src/main/java/com/websearch/WebsearchApplication.java
@@ -0,0 +1,11 @@
+package com.websearch;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WebsearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebsearchApplication.class, args);
+    }
+}
